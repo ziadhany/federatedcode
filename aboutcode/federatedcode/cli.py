7d4ef8938917fcbf293bfe88c62418d196edb013
Keshav Priyadarshi <git@keshav.space>
2024-12-12 18:30:46 +0530

    Add utility and cli to discover PURL on AP server
    Signed-off-by: Keshav Priyadarshi <git@keshav.space>

--- aboutcode/federatedcode/cli.py
+++ aboutcode/federatedcode/cli.py
@@ -10,19 +10,47 @@
 
 import click
 
-from aboutcode.federatedcode.client import get_package_scan
+from aboutcode.federatedcode import client
+
+
+@click.group()
+def handler():
+    pass
 
 
 @click.command()
 @click.argument("purl")
-def handler(purl):
+def scan(purl):
     """
     Get package scan for PURL from FederatedCode git repository.
 
     PURL: PURL to fetch scan result
     """
-    click.echo(get_package_scan(purl=purl))
+    click.echo(client.get_package_scan(purl=purl))
 
+
+@click.command()
+@click.argument("purl")
+def discover(purl):
+    """
+    Discover existing Packages in the FederatedCode AP server.
+
+    PURL: PURL to find in AP server
+    """
+    if response := client.discover_package_in_ap_server(purl=purl):
+        click.echo(click.style(response, fg="green", bold=True))
+    else:
+        click.echo(
+            click.style(
+                f"Error: {purl} not available on {client.FEDERATEDCODE_AP_HOST} AP server.",
+                fg="red",
+                bold=True,
+            )
+        )
+
+
+handler.add_command(scan)
+handler.add_command(discover)
 
 if __name__ == "__main__":
     handler()
