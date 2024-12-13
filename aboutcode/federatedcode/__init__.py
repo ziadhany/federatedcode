f2cf177dfb91a0a4f6f8d5cb89e88d99b659b2c9
Keshav Priyadarshi <git@keshav.space>
2024-12-13 14:39:48 +0530

    Add CHANGELOG for aboutcode.federatedcode
    Signed-off-by: Keshav Priyadarshi <git@keshav.space>

---
+++ aboutcode/federatedcode/__init__.py
@@ -0,0 +1,10 @@
+#
+# Copyright (c) nexB Inc. and others. All rights reserved.
+# FederatedCode is a trademark of nexB Inc.
+# SPDX-License-Identifier: Apache-2.0
+# See http://www.apache.org/licenses/LICENSE-2.0 for the license text.
+# See https://github.com/nexB/federatedcode for support or download.
+# See https://aboutcode.org for more information about AboutCode.org OSS projects.
+#
+
+__version__ = "0.1.0"
